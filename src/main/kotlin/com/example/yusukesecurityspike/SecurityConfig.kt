186a888b89ffcee18b9bb925d18f04f5388d3924
yusukeyokota <tarut8166@gmail.com>
2024-05-21 14:57:30 +0900

    BasicログインをNoOpで実装

--- src/main/kotlin/com/example/yusukesecurityspike/SecurityConfig.kt
+++ src/main/kotlin/com/example/yusukesecurityspike/SecurityConfig.kt
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -19,4 +24,18 @@ class SecurityConfig {
         return http.build()
     }
 
+    @Bean
+    fun userDetailsService(passwordEncoder: PasswordEncoder): UserDetailsService {
+        val user = User.builder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
+    }
 }
